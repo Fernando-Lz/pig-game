d0d210f6f80794aee287fba55e2181cad728ca24
Fernando <flopezlopez6@outlook.com>
2022-03-27 20:43:25 -0600

    :sparkles: Switch current player added

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 // Variables
-const score0Element = document.querySelector('#score--0');
-const score1Element = document.querySelector('#score--1');
-const current0Element = document.querySelector('#current--0');
-const current1Element = document.querySelector('#current--1');
+const score0Element = document.getElementById('score--0');
+const score1Element = document.getElementById('score--1');
+const player0Element = document.querySelector('.player--0');
+const player1Element = document.querySelector('.player--1');
+
 const diceElement = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Initial conditions
 score0Element.textContent = 0;
@@ -17,7 +21,6 @@ diceElement.classList.add('hidden');
 // Functions
 const diceRoll = () => {
   const diceNumber = Math.trunc(Math.random() * 6) + 1;
-
   // Display dice
   diceElement.classList.remove('hidden');
   diceElement.src = `img/dice-${diceNumber}.png`;
@@ -25,7 +28,14 @@ const diceRoll = () => {
   // Check for diceNumber 1
   if (diceNumber !== 1) {
     currentScore += diceNumber;
-    current0Element.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+  } else {
+    // Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0Element.classList.toggle('player--active');
+    player1Element.classList.toggle('player--active');
   }
 };
 
